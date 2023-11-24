d9f03717d70d4369d26d623444bc8fb35d94839e
saurabhjaykar1603 <jaykarsaurabh97@gmail.com>
2023-11-24 15:38:00 +0530

    feat(api): add POST endpoint for creating transactions

--- server/index.js
+++ server/index.js
@@ -3,6 +3,8 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 dotenv.config();
 
+import Transaction from "./models/Transaction.js";
+
 const app = express();
 
 app.use(express.json());
@@ -13,6 +15,30 @@ const connDB = async () => {
     console.log(" Mongodb connected successfully ");
   }
 };
+
+// post : /api/transactions
+app.post("/api/transactions", async (req, res) => {
+  const { amount, transactionType, category, description } = req.body;
+  const transaction = new Transaction({
+    amount,
+    transactionType,
+    category: category || "other",
+    description,
+  });
+  try {
+    const savedTransaction = await transaction.save();
+    res.json({
+      success: true,
+      data: savedTransaction,
+      message: "Transaction saved successfully",
+    });
+  } catch (err) {
+    res.json({
+      success: false,
+      message: err.message,
+    });
+  }
+});
 
 app.get("/api/health", (req, res) => {
   res.json({
