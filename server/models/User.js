8f765342d6e8724f581e41eb3e721c075bd4b963
saurabhjaykar1603 <jaykarsaurabh97@gmail.com>
2023-11-28 10:23:28 +0530

    ref : User schema

--- server/models/User.js
+++ server/models/User.js
@@ -1,42 +1,46 @@
 import { Schema, model } from "mongoose";
 
-const userSchema = new Schema({
-  userName: {
-    type: String,
-    required: true,
-    unique: true,
-    lowercase: true,
+const userSchema = new Schema(
+  {
+    userName: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      lowercase: true,
+    },
+    bankName: {
+      type: String,
+      enum: [
+        "kotak",
+        "maharashtra-bank",
+        "sbi",
+        "hdfc",
+        "axis",
+        "badoda",
+        "other",
+      ],
+      required: true,
+    },
+    city: {
+      type: String,
+      required: true,
+    },
   },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-    lowercase: true,
-  },
-  password: {
-    type: String,
-    required: true,
-    unique: true,
-    lowercase: true,
-  },
-  bankName: {
-    type: String,
-    enum: [
-      "kotak",
-      "maharashtra-bank",
-      "sbi",
-      "hdfc",
-      "axis",
-      "badoda",
-      "other",
-    ],
-    required: true,
-  },
-  city: {
-    type: String,
-    required: true,
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
 const User = model("User", userSchema);
 export default User;
