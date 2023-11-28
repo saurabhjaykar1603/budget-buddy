448c3b56a6f692f7bde76a4705d37f912e361259
saurabhjaykar1603 <jaykarsaurabh97@gmail.com>
2023-11-28 09:37:38 +0530

    chore : User schems and model created

---
+++ server/models/User.js
@@ -0,0 +1,42 @@
+import { Schema, model } from "mongoose";
+
+const userSchema = new Schema({
+  userName: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+  },
+  bankName: {
+    type: String,
+    enum: [
+      "kotak",
+      "maharashtra-bank",
+      "sbi",
+      "hdfc",
+      "axis",
+      "badoda",
+      "other",
+    ],
+    required: true,
+  },
+  city: {
+    type: String,
+    required: true,
+  },
+});
+
+const User = model("User", userSchema);
+export default User;
