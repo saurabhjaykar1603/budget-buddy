f1010a3d82a00d250612d119bd6b86789c35fd6d
saurabhjaykar1603 <jaykarsaurabh97@gmail.com>
2023-12-01 11:38:56 +0530

    feat: Implement user login and logout functionality at navbar component

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,8 +1,18 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import "./Navbar.css"
 
 const Navbar = () => {
   const [isMobileMenuOpen, setMobileMenuOpen] = useState(false);
+  const [user, setUser] = useState({});
+
+  useEffect(() => {
+    const getloggedInUsersFromLocalStorage = JSON.parse(
+      localStorage.getItem("user" || "{}")
+    );
+    console.log(getloggedInUsersFromLocalStorage);
+    setUser(getloggedInUsersFromLocalStorage);
+  }, []);
 
   const handleMobileMenuToggle = () => {
     setMobileMenuOpen(!isMobileMenuOpen);
@@ -46,7 +56,28 @@ const Navbar = () => {
             <Link to="/login" className="text-dark  hover:text-red-700">
               Login{" "}
             </Link>
-            <Link className="text-dark">User 👋 </Link>
+            <span className="text-dark cursor-pointer">
+              Hello {user?.userName || "User "} ! 👋
+            </span>
+            {user?.userName ? (
+              <span
+                className="pointer login-links fw-bold fs-5 ms-2 cursor-pointer"
+                onClick={() => {
+                  localStorage.removeItem("user");
+                  window.location.href = "/login";
+                }}
+              >
+                <button className="Btn-logout">
+                  <div className="sign">
+                    <svg viewBox="0 0 512 512">
+                      <path d="M377.9 105.9L500.7 228.7c7.2 7.2 11.3 17.1 11.3 27.3s-4.1 20.1-11.3 27.3L377.9 406.1c-6.4 6.4-15 9.9-24 9.9c-18.7 0-33.9-15.2-33.9-33.9l0-62.1-128 0c-17.7 0-32-14.3-32-32l0-64c0-17.7 14.3-32 32-32l128 0 0-62.1c0-18.7 15.2-33.9 33.9-33.9c9 0 17.6 3.6 24 9.9zM160 96L96 96c-17.7 0-32 14.3-32 32l0 256c0 17.7 14.3 32 32 32l64 0c17.7 0 32 14.3 32 32s-14.3 32-32 32l-64 0c-53 0-96-43-96-96L0 128C0 75 43 32 96 32l64 0c17.7 0 32 14.3 32 32s-14.3 32-32 32z"></path>
+                    </svg>
+                  </div>
+
+                  <div className="text">Logout</div>
+                </button>
+              </span>
+            ) : null}
           </div>
 
           {/* Mobile Navbar Toggle */}
@@ -114,9 +145,9 @@ const Navbar = () => {
           >
             Login{" "}
           </Link>
-          <Link className="block py-2 text-dark  hover:text-red-700">
-            User 👋{" "}
-          </Link>
+          <p className="block py-2 text-dark  hover:text-red-700 cursor-pointer">
+            Hello {user?.userName || "Guest "} !{" "}
+          </p>
         </ul>
       </div>
     </>
