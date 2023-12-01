f871b9af49c208a6545546b75738f59b6a1d6e01
saurabhjaykar1603 <jaykarsaurabh97@gmail.com>
2023-12-01 10:18:52 +0530

    chore : import useEffect hook

--- client/src/views/Signup/Signup.jsx
+++ client/src/views/Signup/Signup.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Navbar from "../../components/Navbar/Navbar";
 import "./Signup.css";
 import { Link } from "react-router-dom";
