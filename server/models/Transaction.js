b201617917bc51dc3d5a817c0815650eaa4bc439
saurabhjaykar1603 <jaykarsaurabh97@gmail.com>
2023-12-02 12:50:23 +0530

    ref : transaction schema add timestamp

--- server/models/Transaction.js
+++ server/models/Transaction.js
@@ -1,40 +1,45 @@
 import { Schema, model } from "mongoose";
 
-const TransactionSchema = new Schema({
-  user: {
-    type: Schema.Types.ObjectId,
-    ref: "User",
-    required: true,
+const TransactionSchema = new Schema(
+  {
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    amount: {
+      type: Number,
+      required: true,
+    },
+    transactionType: {
+      type: String,
+      enum: ["credit", "debit"],
+      required: true,
+    },
+    category: {
+      type: String,
+      enum: [
+        "food",
+        "entertainment",
+        "travel",
+        "shopping",
+        "education",
+        "other",
+        "freelancing",
+        "salary",
+        "gift",
+      ],
+      default: "other",
+    },
+    description: {
+      type: String,
+      required: true,
+    },
   },
-  amount: {
-    type: Number,
-    required: true,
-  },
-  transactionType: {
-    type: String,
-    enum: ["credit", "debit"],
-    required: true,
-  },
-  category: {
-    type: String,
-    enum: [
-      "food",
-      "entertainment",
-      "travel",
-      "shopping",
-      "education",
-      "other",
-      "freelancing",
-      "salary",
-      "gift",
-    ],
-    default: "other",
-  },
-  description: {
-    type: String,
-    required: true,
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
 const Transaction = model("Transaction", TransactionSchema);
 export default Transaction;
